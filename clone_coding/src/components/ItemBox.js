2255626b999f2c849cd7209b1934af39b00f10e4
y0ung22 <amy6484@ewhain.net>
2023-09-09 19:23:46 +0900

    asset: 테스트 이미지 추가

--- clone_coding/src/components/ItemBox.js
+++ clone_coding/src/components/ItemBox.js
@@ -2,8 +2,28 @@ import React from "react";
 import { styled } from "styled-components";
 import { useNavigate } from "react-router-dom";
 
+import testImg from "../assets/images/test.jpg";
+
 const ItemBox = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  return (
+    <Wrapper>
+      <ItemImage src={testImg} />
+    </Wrapper>
+  );
 };
 
 export default ItemBox;
+
+const Wrapper = styled.div`
+  width: 223px;
+  height: 330px;
+`;
+
+const ItemImage = styled.img`
+  width: 223px;
+  height: 223px;
+  border-radius: 10px;
+  object-fit: cover;
+`;
