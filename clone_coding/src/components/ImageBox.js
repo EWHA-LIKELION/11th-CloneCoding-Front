47df3c0007c56f0eb2e46203a61e50b7ab03344b
y0ung22 <amy6484@ewhain.net>
2023-09-11 17:15:09 +0900

    feat: ItemBox 컴포넌트 생성

---
+++ clone_coding/src/components/ImageBox.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { styled } from "styled-components";
+
+import testImg from "../assets/images/test.jpg";
+import before from "../assets/icons/before.svg";
+import next from "../assets/icons/next.svg";
+
+const ImageBox = () => {
+  return (
+    <Wrapper>
+      <BeforeBtn>
+        <img src={before} />
+      </BeforeBtn>
+      <Image src={testImg} />
+      <NextBtn>
+        <img src={next} />
+      </NextBtn>
+    </Wrapper>
+  );
+};
+
+export default ImageBox;
+
+const Wrapper = styled.div`
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+`;
+
+const BeforeBtn = styled.div`
+  height: 25px;
+  cursor: pointer;
+`;
+
+const Image = styled.img`
+  margin: 0px 15px;
+  width: 660px;
+  height: 500px;
+  border-radius: 10px;
+  object-fit: cover;
+`;
+
+const NextBtn = styled.div`
+  height: 25px;
+  cursor: pointer;
+`;
--- clone_coding/src/pages/DetailPage.js
+++ clone_coding/src/pages/DetailPage.js
@@ -2,11 +2,13 @@ import React from "react";
 import { styled } from "styled-components";
 
 import TopBar from "../components/TopBar";
+import ImageBox from "../components/ImageBox";
 
 const DetailPage = () => {
   return (
     <Wrapper>
       <TopBar />
+      <ImageBox />
     </Wrapper>
   );
 };
@@ -16,5 +18,6 @@ export default DetailPage;
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   margin: auto;
 `;
