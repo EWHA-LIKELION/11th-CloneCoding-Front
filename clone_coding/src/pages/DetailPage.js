3d93c20aa344ee456528ca89116da8b2bdc5bc11
y0ung22 <amy6484@ewhain.net>
2023-09-11 16:45:07 +0900

    asset: 프로필 이미지 추가

--- clone_coding/src/pages/DetailPage.js
+++ clone_coding/src/pages/DetailPage.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { styled } from "styled-components";
 
+import TopBar from "../components/TopBar";
+
 const DetailPage = () => {
-  return <></>;
+  return (
+    <Wrapper>
+      <TopBar />
+    </Wrapper>
+  );
 };
 
 export default DetailPage;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+`;
