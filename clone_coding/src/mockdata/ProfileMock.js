525fd2019515f51a207406569215cf003ac76e4a
kyubinjin <2376282@ewhain.net>
2023-09-21 02:11:34 +0900

    style: css 수정 및 불필요 파일 삭제

--- clone_coding/src/mockdata/ProfileMock.js
+++ clone_coding/src/mockdata/ProfileMock.js
@@ -1,28 +1,34 @@
 import defaultImg from "../assets/profile/defaultImg.png";
+import img1 from "../assets/profile/img1.jpg";
+import img2 from "../assets/profile/img2.jpg";
+import img3 from "../assets/profile/img3.jpg";
+import img4 from "../assets/profile/img4.jpg";
+import img5 from "../assets/profile/img5.jpg";
+import img6 from "../assets/profile/img6.jpg";
 
 export const ProfileMock = [
+  img1,
+  defaultImg,
+  img5,
+  defaultImg,
+  img2,
+  img5,
+  defaultImg,
+  img4,
+  defaultImg,
+  img6, //10
+  defaultImg,
+  img3,
   defaultImg,
   defaultImg,
   defaultImg,
   defaultImg,
-  defaultImg,
-  defaultImg,
-  defaultImg,
-  defaultImg,
-  defaultImg,
-  defaultImg, //10
-  defaultImg,
-  defaultImg,
-  defaultImg,
-  defaultImg,
-  defaultImg,
-  defaultImg,
-  defaultImg,
+  img6,
   defaultImg,
   defaultImg,
   defaultImg, //20
   defaultImg,
-  defaultImg,
+  img1,
   defaultImg,
   defaultImg,
   defaultImg,
