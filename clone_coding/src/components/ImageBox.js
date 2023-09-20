df7abe37ad7cac52667fa306d6097de3a9916793
y0ung22 <amy6484@ewhain.net>
2023-09-20 18:17:28 +0900

    feat: 이미지 슬라이드 구현

--- clone_coding/src/components/ImageBox.js
+++ clone_coding/src/components/ImageBox.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { styled } from "styled-components";
 
 import before from "../assets/icons/before.svg";
@@ -6,15 +6,53 @@ import next from "../assets/icons/next.svg";
 
 const ImageBox = ({ id, images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const slideRef = useRef(null);
 
-  //다음 이미지로 전환
-  const nextClick = () => {};
+  //이미지 index가 시작과 끝일 때 이미지 이동
+  useEffect(() => {
+    if (currentIndex === -1) {
+      slideRef.current.style.transition = "all 0.5s ease-in-out";
+      slideRef.current.style.transform = `translateX(-${
+        (images.length - 1) * 660
+      }px)`;
+      setCurrentIndex(images.length - 1);
+    } else if (currentIndex === images.length) {
+      setCurrentIndex(0);
+      slideRef.current.style.transition = "all 0.5 ease-in-out";
+      slideRef.current.style.transform = `translateX(0px)`;
+    }
+  }, [currentIndex]);
 
   //이전 이미지로 전환
-  const beforeClick = () => {};
+  const beforeClick = () => {
+    if (currentIndex === -1) {
+      setCurrentIndex(images.length - 1);
+    } else {
+      setCurrentIndex((prev) => prev - 1);
+      slideRef.current.style.transition = "all 0.5s ease-in-out";
+      slideRef.current.style.transform = `translateX(-${
+        currentIndex * 660 - 660
+      }px)`;
+    }
+  };
+
+  //다음 이미지로 전환
+  const nextClick = () => {
+    if (currentIndex === images.length) {
+      setCurrentIndex(0);
+    } else {
+      setCurrentIndex((prev) => prev + 1);
+      slideRef.current.style.transition = "all 0.5s ease-in-out";
+      slideRef.current.style.transform = `translateX(-${
+        660 * (currentIndex + 1)
+      }px)`;
+    }
+  };
 
   //이미지가 1개일 경우 화살표 띄우지 않도록 조건 지정
   const hideBtn = images.length === 1;
+
+  console.log(images);
 
   return (
     <Wrapper>
@@ -23,11 +61,13 @@ const ImageBox = ({ id, images }) => {
           <img src={before} />
         </BeforeBtn>
       )}
-      <ImageSlider>
-        {images.map((image) => (
-          <Image src={image} />
-        ))}
-      </ImageSlider>
+      <ImageWrapper>
+        <ImageSlider ref={slideRef}>
+          {images.map((image) => (
+            <img src={image} />
+          ))}
+        </ImageSlider>
+      </ImageWrapper>
       <ImageCnt></ImageCnt>
       {!hideBtn && (
         <NextBtn onClick={nextClick}>
@@ -44,28 +84,38 @@ const Wrapper = styled.div`
   margin-top: 80px;
   display: flex;
   align-items: center;
+  overflow: hidden;
 `;
 
 const BeforeBtn = styled.div`
   height: 25px;
   margin-right: 15px;
   cursor: pointer;
+  z-index: 50;
 `;
 
-const ImageSlider = styled.div`
+const ImageWrapper = styled.div`
+  display: flex;
+  position: relative;
   width: 660px;
   height: 500px;
   border-radius: 10px;
   overflow: hidden;
 `;
 
-const Image = styled.img`
-  position: relative;
-  border-radius: 10px;
+const ImageSlider = styled.div`
+  float: left;
   width: 660px;
   height: 500px;
-  float: left;
-  object-fit: cover;
+  border-radius: 10px;
+  overflow: hidden;
+  z-index: 20;
+  img {
+    width: 660px;
+    height: 500px;
+    border-radius: 10px;
+    object-fit: cover;
+  }
 `;
 
 const ImageCnt = styled.div`
@@ -89,4 +139,5 @@ const NextBtn = styled.div`
   height: 25px;
   margin-left: 15px;
   cursor: pointer;
+  z-index: 50;
 `;
